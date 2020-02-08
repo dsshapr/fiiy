3f20477f043f74dfaab9ef2943475163d5f85fd6
MoveAngel <moveangel29@gmail.com>
2020-02-09 00:27:50 +0700

    Trying fix everytime

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -120,7 +120,7 @@ async def carbon_api(e):
     await e.delete()  # Deleting msg
 
 
-@register(outgoing=True, pattern="^.img (.*)")
+@register(outgoing=True, pattern="^.img(?: |$)(.*)")
 async def img_sampler(event):
     """ For .img command, search and return images matching the query. """
     await event.edit("Processing...")
@@ -131,7 +131,7 @@ async def img_sampler(event):
         lim = lim.replace("lim=", "")
         query = query.replace("lim=" + lim[0], "")
     except IndexError:
-        lim = 3
+        lim = 6
     response = google_images_download.googleimagesdownload()
 
     # creating list of arguments
@@ -145,8 +145,8 @@ async def img_sampler(event):
     # passing the arguments to the function
     paths = response.download(arguments)
     lst = paths[0][query]
-    await event.client.send_file(
-        await event.client.get_input_entity(event.chat_id), lst)
+    await bot.send_file(
+        await bot.get_input_entity(event.chat_id), lst)
     shutil.rmtree(os.path.dirname(os.path.abspath(lst[0])))
     await event.delete()
 

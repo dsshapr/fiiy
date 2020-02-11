68f3594cf4d22ba327a1b8d6c9e0f7759b286a1f
MoveAngel <moveangel29@gmail.com>
2020-02-11 10:32:00 +0700

    Revert "Publish new command"
    This reverts commit 30627bfe1488b22d60a641900c1fb20f3b13e750.

--- userbot/modules/memes.py
+++ userbot/modules/memes.py
@@ -1234,33 +1234,6 @@ async def taco(e):
                      "\n( >🌮 Want a taco?")
 
 
-@register(pattern=r".hack(?: |$)(.*)", outgoing=True)
-async def _(event):
-    if event.fwd_from:
-        return
-    animation_interval = 2
-    animation_ttl = range(0, 11)
-    input_str = event.pattern_match.group(1)
-    if input_str == "hack":
-        await event.edit(input_str)
-        animation_chars = [
-            "`Connecting To Hacked Private Server...`",
-            "`Target Selected.`",
-            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
-            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
-            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
-            "`Hacking... 100%\n█████████HACKED███████████ `",
-            "`Targeted Account Hacked...\n\nPay @MoveAngel 6969$ or Pekob To Remove This Hack`"
-        ]
-        for i in animation_ttl:
-            await asyncio.sleep(animation_interval)
-            await event.edit(animation_chars[i % 11])
-
-
 @register(outgoing=True, pattern="^.paw$")  
 async def paw(e):
    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
